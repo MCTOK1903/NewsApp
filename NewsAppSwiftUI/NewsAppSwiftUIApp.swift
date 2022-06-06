d055a7278520c5bd4edc56cec0e3a9e143438999
Muhammed Celal Tok <celal.tok@hepsiburada.com>
2022-06-06 23:28:10 +0300

    Initial Commit

---
+++ NewsAppSwiftUI/NewsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppSwiftUIApp.swift
+//  NewsAppSwiftUI
+//
+//  Created by Muhammed Celal Tok on 6.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
